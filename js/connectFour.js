0732203ff3692e34ba0e905e1fc7d69d93f49794
sh999 <satrio.h@gmail.com>
2015-05-30 10:34:50 -0400

    Hover on unfilled cell

--- js/connectFour.js
+++ js/connectFour.js
@@ -1,14 +1,38 @@
+var fieldStatus = [
+	["empty","empty","empty"],
+	["empty","empty","empty"],
+	["empty","empty","empty"]
+];
+
 function colorCell(cell){
 	var row = $(cell).data("row");
 	var col = $(cell).data("col");
-	$("#box22").addClass("clicked");	// Figure out how to drop piece in correct cell
+	$(cell).addClass("clicked");	// Figure out how to drop piece in correct cell
+	fieldStatus[row][col] = "filled";
+}
+
+function getFreeCell(cell){
+	var row = 2;
+	var col = $(cell).data("col");
+	for(i = 0; i <= 2; i++){
+		if(fieldStatus[row][col] === "filled"){
+			row = row - 1;
+		}
+	}
+	return "#box"+row+col;
+}
+
+function getFreeRow(row){
+
+	return "2";
 }
 
 $(document).ready(function() {
 	$(".cell").hover(function () {
-		$("#box22").addClass("hover");	// Figure out how to highlight bottom-most cell of each column
+		freeCell = getFreeCell(this);
+		$(freeCell).addClass("hover");	// Figure out how to highlight bottom-most cell of each column
 	}, function(){
-		$("#box22").removeClass("hover");
+		$(freeCell).removeClass("hover");
 	}); // End hover
 
 	$(".cell").click(function(){
