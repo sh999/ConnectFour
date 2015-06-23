76498928e156227c460a889517da311efb2faf51
sh999 <satrio.h@gmail.com>
2015-06-22 23:11:33 -0400

    Working on checkDown scenario

--- js/connectFour.js
+++ js/connectFour.js
@@ -47,14 +47,30 @@ function getFreeCell(cell){
 	return "#box"+row+col;
 }
 
+function checkDown(a, b, c){
+	var i = 2;
+	$("#generalLog").text();
+	return i;
+}
+
 function checkConditions(cell){	// Check if anyone has won, and if not, keep playing
 	var placedRow = $(cell).data("row");
 	var placedCol = $(cell).data("col");
+	var checkedRow = placedRow - 1; //	The first row that will be checked first will be the one below where piece was placed
+	var checkedCol = placedCol;	// The first column that will be checked first will be the same one where piece was placed
 	// Check consecutive pieces under
 	var keepLooping = true;
-	consecutive = 0;
-	while(keepLooping === true){
-		placedRow = placedRow + 1;
+	var consecutive = 0;
+	var consecutiveSouth = 1; // # of consecutive pieces from placed piece to pieces underneath.  Default is 1 because the placed piece is counted
+	
+	whil(keepLooping === true){
+		
+		if(placedRow == fieldStatus.length-1){	// If dropped piece is at bottom
+			keepLooping = false;
+		}
+		else{
+			checkDown(placedRow, placedCol, fieldStatus.length);
+		}
 		testCell = "#box"+placedRow+placedCol;
 		/*
 		//Something is keeping the loop too long here
@@ -64,21 +80,14 @@ function checkConditions(cell){	// Check if anyone has won, and if not, keep pla
 		}
 		else{
 
-		}
-		/*
-		if(5>3){
-			$("#log").text("boo");
-		}*/ 	 	
-		// if(r === 3){*/
-		if(placedRow == fieldStatus.length-1){
-			keepLooping = false;
-		}
+		} 	 	
+		*/
 		keepLooping = false;
-
 		$("#logLoop").text("looping");
 	}
 	$("#logEndLoop").text("looping end");
-	$("#logPlacement").text("placement = " +placedRow+" "+placedCol + " " + ", consecutive = " + consecutive);
+	$("#logPlacement").text("placement = " +placedRow+" "+placedCol + " " + ", consecutive = " + consecutive + "field length = " + fieldStatus.length);
+	checkDown();
 	turn.changeTurn();
 }
 
